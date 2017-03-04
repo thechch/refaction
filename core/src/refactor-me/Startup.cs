9b0668a4665b3f29e304062b378acc01437eab04
Anton Serous <thech.ch@gmail.com>
2017-03-04 22:30:11 +1300

    Startup configuration included

--- core/src/refactor-me/Startup.cs
+++ core/src/refactor-me/Startup.cs
@@ -1,15 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace refactor_me
 {
-    public class Startup {
-        public Startup(IHostingEnvironment env) {
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddEnvironmentVariables();
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
             Configuration = builder.Build();
         }
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                    .AddJsonOptions(o =>
+                    {
+                        o.SerializerSettings.Formatting = Formatting.Indented;
+                    });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
     }
 }
