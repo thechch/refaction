64d7f75c63a6664d1855783bf7acfca70649c7de
Anton Serous <anton.serous@icg.co.nz>
2017-03-14 17:35:50 +1300

    tests skeleton added

---
+++ test/Xero.RefactorMe.Data.Tests/IntegrationTests/ProductOptionRepositoryTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xero.RefactorMe.Data.Tests.IntegrationTests
+{
+    class ProductOptionRepositoryTests
+    {
+    }
+}
---
+++ test/Xero.RefactorMe.Web.Tests/UnitTests/ProductsControllerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xero.RefactorMe.Web.Tests.UnitTests
+{
+    class ProductsControllerTests
+    {
+
+    }
+}
