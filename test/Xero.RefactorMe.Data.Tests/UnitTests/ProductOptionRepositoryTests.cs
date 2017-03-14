9b95e576987c61e591ce8a3e954fda23bdb23d7f
Anton Serous <thech.ch@gmail.com>
2017-03-15 01:24:49 +1300

    Unit tests for repositories added

---
+++ test/Xero.RefactorMe.Data.Tests/UnitTests/ProductOptionRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xero.RefactorMe.Data.Abstract;
+using Xero.RefactorMe.Model;
+using Xunit;
+
+namespace Xero.RefactorMe.Data.Tests.UnitTests {
+
+    public class ProductOptionRepositoryTests {
+
+        /// <summary>
+        /// Sets up a fixture class to use across all the tests
+        /// </summary>
+        public class ProductOptionRepositoryFixture : IDisposable {
+
+            public readonly IProductOptionRepository _productOptionRepository;
+
+            public ProductOptionRepositoryFixture () {
+                var testProductOptions = new List<ProductOption> {
+                    new ProductOption { Id = new Guid("5c2996ab-54ad-4999-92d2-89245682d534"), Name = "Test option 1" },
+                    new ProductOption { Id = new Guid("9ae6f477-a010-4ec9-b6a8-92a85d6c5f03"), Name = "Test option 2" },
+                }.AsQueryable ();
+
+                var mockSet = new Mock<DbSet<ProductOption>> ();
+                mockSet.As<IQueryable<ProductOption>> ().Setup (m => m.Provider).Returns (testProductOptions.Provider);
+                mockSet.As<IQueryable<ProductOption>> ().Setup (m => m.Expression).Returns (testProductOptions.Expression);
+                mockSet.As<IQueryable<ProductOption>> ().Setup (m => m.ElementType).Returns (testProductOptions.ElementType);
+                mockSet.As<IQueryable<ProductOption>> ().Setup (m => m.GetEnumerator ()).Returns (testProductOptions.GetEnumerator ());
+
+                var mockContext = new Mock<RefactorMeDbContext> ();
+                mockContext.Setup (c => c.ProductOptions).Returns (mockSet.Object);
+
+                _productOptionRepository = new ProductOptionRepository(mockContext.Object);
+            }
+            public void Dispose()
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// This class has no code, and is never created. Its purpose is simply
+        /// to be the place to apply [CollectionDefinition] and all the
+        /// ICollectionFixture<> interfaces.
+        /// </summary>
+		[CollectionDefinition("ProductOptionRepository")]
+		public class ProductOptionRepositoryCollection : ICollectionFixture<ProductOptionRepositoryFixture>
+        { 
+
+        }
+
+        /// <summary>
+        /// Tests for GetSingle(Guid id)
+        /// </summary>
+        [Collection("ProductOptionRepository")]
+        public class GetSingleMethodShould
+        {
+            private readonly ProductOptionRepositoryFixture _fixture;
+
+			public GetSingleMethodShould(ProductOptionRepositoryFixture fixture)
+			{
+				_fixture = fixture;
+			}
+
+            [Theory]
+            [InlineData("5c2996ab-54ad-4999-92d2-89245682d534")]
+            [InlineData("9ae6f477-a010-4ec9-b6a8-92a85d6c5f03")]
+            public void ReturnAListOfProducts(string id)
+            {
+                var optionId = new Guid(id);
+                var productOption = _fixture._productOptionRepository.GetSingle(optionId);
+
+                Assert.NotNull(productOption);
+                Assert.IsType<ProductOption>(productOption);
+            }
+
+            [Fact]
+            public void ReturnNullWhenProductOptionHasNoSuchId(string name)
+            {
+                var guid = new Guid();
+                var productOption = _fixture._productOptionRepository.GetSingle(guid);
+
+                Assert.Null(productOption);
+            }
+        }
+    }
+}
---
+++ test/Xero.RefactorMe.Data.Tests/UnitTests/ProductRepositoryTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xero.RefactorMe.Data.Abstract;
+using Xero.RefactorMe.Model;
+using Xunit;
+
+namespace Xero.RefactorMe.Data.Tests.UnitTests {
+    public class ProductRepositoryTests {
+
+        /// <summary>
+        /// Sets up a fixture class to use across all the tests
+        /// </summary>
+        public class ProductRepositoryFixture : IDisposable {
+
+            public readonly IProductRepository _productRepository;
+
+            public ProductRepositoryFixture () {
+                var testProducts = new List<Product> {
+                    new Product { Name = "Test product 1" },
+                    new Product { Name = "Test product 2" },
+                }.AsQueryable ();
+
+                var mockSet = new Mock<DbSet<Product>> ();
+                mockSet.As<IQueryable<Product>> ().Setup (m => m.Provider).Returns (testProducts.Provider);
+                mockSet.As<IQueryable<Product>> ().Setup (m => m.Expression).Returns (testProducts.Expression);
+                mockSet.As<IQueryable<Product>> ().Setup (m => m.ElementType).Returns (testProducts.ElementType);
+                mockSet.As<IQueryable<Product>> ().Setup (m => m.GetEnumerator ()).Returns (testProducts.GetEnumerator ());
+
+                var mockContext = new Mock<RefactorMeDbContext> ();
+                mockContext.Setup (c => c.Products).Returns (mockSet.Object);
+
+                _productRepository = new ProductRepository (mockContext.Object);
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// This class has no code, and is never created. Its purpose is simply
+        /// to be the place to apply [CollectionDefinition] and all the
+        /// ICollectionFixture<> interfaces.
+        /// </summary>
+		[CollectionDefinition("ProductRepository")]
+		public class ProductRepositoryCollection : ICollectionFixture<ProductRepositoryFixture>
+        { 
+
+        }
+
+        /// <summary>
+        /// Tests for GetByName(string name)
+        /// </summary>
+        [Collection("ProductRepository")]
+        public class GetByNameMethodShould
+        {
+            private readonly ProductRepositoryFixture _fixture;
+
+			public GetByNameMethodShould(ProductRepositoryFixture fixture)
+			{
+				_fixture = fixture;
+			}
+
+            [Theory]
+            [InlineData("Test product 1")]
+            [InlineData("Test product 2")]
+            public void ReturnAListOfProducts(string name)
+            {
+                var listOfProducts = _fixture._productRepository.GetByName(name);
+
+                Assert.IsType<List<Product>>(listOfProducts);
+            }
+        }
+
+        /// <summary>
+        /// Tests for GetAll()
+        /// </summary>
+        [Collection("ProductRepository")]
+        public class GetAllMethodShould
+        {
+            private readonly ProductRepositoryFixture _fixture;
+
+			public GetAllMethodShould(ProductRepositoryFixture fixture)
+			{
+				_fixture = fixture;
+			}
+
+            [Fact]
+            public void ReturnAListOfProducts(string name)
+            {
+                var allProducts = _fixture._productRepository.GetAll();
+
+                Assert.IsNotType<IEnumerable<ProductOption>>(allProducts);
+            }
+        }
+    }
+}
